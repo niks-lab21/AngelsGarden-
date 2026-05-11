dd7a52d680340109cd07f9c8ee3781024901ef57
niks-lab21 <276241943+niks-lab21@users.noreply.github.com>
2026-05-11 09:40:12 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/nikudev/angelsgarden/MainActivity.java
+++ android/app/src/main/java/com/nikudev/angelsgarden/MainActivity.java
@@ -1,5 +1,19 @@
-package com.nikudev.angelsgarden;
+package com.niks.angelsgarden;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onWebViewCreated(WebView webView) {
+        super.onWebViewCreated(webView);
+        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+    }
+}
