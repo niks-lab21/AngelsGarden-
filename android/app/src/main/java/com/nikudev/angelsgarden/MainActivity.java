d33f73c82cead81bb51d50a2cf762fab8bca4a4c
niks-lab21 <276241943+niks-lab21@users.noreply.github.com>
2026-05-12 13:29:28 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/nikudev/angelsgarden/MainActivity.java
+++ android/app/src/main/java/com/nikudev/angelsgarden/MainActivity.java
@@ -7,7 +7,5 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // DO NOT touch the WebView here — Capacitor handles it
-        // If you need media autoplay, use a Capacitor plugin instead
     }
 }
